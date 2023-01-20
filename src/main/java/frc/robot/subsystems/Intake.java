802e23b8c51f90f8b17b4cad5c6663bb996d7554
ehernandez6800 <ehernandez6800@stu.neisd.net>
2023-01-19 18:54:19 -0600

    Starting to code the Intake Subsystem.

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class Intake extends SubsystemBase {
+    private final MotorControllerGroup intakeMotors =
+        new MotorControllerGroup(null, null)
+}
