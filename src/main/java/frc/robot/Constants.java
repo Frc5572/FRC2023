0aa09538d0d55e4da7f3f8d5c6529f876febe3cc
ihernandez3647 <114842174+ihernandez3647@users.noreply.github.com>
2023-01-22 17:25:19 -0600

    Finished.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import frc.lib.math.SwerveModuleConstants;
+import edu.wpi.first.math.util.Units;
 
 /**
  * Constants file.
@@ -35,43 +36,43 @@ public final class Constants {
         public static final double limelightAngle = 0;
     }
     public static class ModuleZero {
-        public static final int angleMotorID = 0;
-        public static final int driveMotorID = 0;
-        public static final int CANCoderID = 0;
-        public static final double angleOffsetID = 0.0;
+        public static final int angleMotorID = 50;
+        public static final int driveMotorID = 8;
+        public static final int CANCoderID = 3;
+        public static final double angleOffsetID = 138.428;
         public static final SwerveModuleConstants Constants =
             new SwerveModuleConstants(angleMotorID, driveMotorID, CANCoderID, driverID);
     }
     public static class ModuleOne {
-        public static final int angleMotorID = 0;
-        public static final int driveMotorID = 0;
-        public static final int CANCoderID = 0;
-        public static final double angleOffsetID = 0.0;
+        public static final int angleMotorID = 51;
+        public static final int driveMotorID = 52;
+        public static final int CANCoderID = 2;
+        public static final double angleOffsetID = 87.891;
         public static final SwerveModuleConstants Constants =
             new SwerveModuleConstants(angleMotorID, driveMotorID, CANCoderID, driverID);
     }
     public static class ModuleTwo {
-        public static final int angleMotorID = 0;
-        public static final int driveMotorID = 0;
-        public static final int CANCoderID = 0;
-        public static final double angleOffsetID = 0.0;
+        public static final int angleMotorID = 4;
+        public static final int driveMotorID = 2;
+        public static final int CANCoderID = 1;
+        public static final double angleOffsetID = 259.541;
         public static final SwerveModuleConstants Constants =
             new SwerveModuleConstants(angleMotorID, driveMotorID, CANCoderID, driverID);
     }
     public static class ModuleThree {
-        public static final int angleMotorID = 0;
-        public static final int driveMotorID = 0;
-        public static final int CANCoderID = 0;
-        public static final double angleOffsetID = 0.0;
+        public static final int angleMotorID = 3;
+        public static final int driveMotorID = 53;
+        public static final int CANCoderID = 4;
+        public static final double angleOffsetID = 32.695;
         public static final SwerveModuleConstants Constants =
             new SwerveModuleConstants(angleMotorID, driveMotorID, CANCoderID, driverID);
     }
-
-    public static final double trackWidth = 0.0;
-    public static final double wheelBase = 0.0;
+    public static final double maxSpeed = 4;
+    public static final double trackWidth = Units.inchesToMeters(27);
+    public static final double wheelBase = Units.inchesToMeters(27);
     public static SwerveDriveKinematics swerveDriveKinematics = new SwerveDriveKinematics(
-        new Translation2d(wheelBase, trackWidth), new Translation2d(-wheelBase, trackWidth),
-        new Translation2d(wheelBase, -trackWidth), new Translation2d(-wheelBase, -trackWidth));
-
+        new Translation2d(wheelBase / 2.0, trackWidth / 2.0), new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+        new Translation2d(wheelBase / 2.0, -trackWidth / 2.0), new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+    public static final double maxAngularVelocity = 2;
 
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystems.Swerve;
+import frc.robot.commands.TeleopSwerve;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,7 +28,7 @@ public class RobotContainer {
     boolean fieldRelative;
     boolean openLoop;
 
-
+    Swerve swerve = new Swerve();
     /* Subsystems */
 
     /**
@@ -35,6 +37,7 @@ public class RobotContainer {
     public RobotContainer() {
         SmartDashboard.putData("Choose Auto: ", autoChooser);
         autoChooser.setDefaultOption("Do Nothing", new WaitCommand(1));
+        swerve.setDefaultCommand(new TeleopSwerve(true, 0, false, swerve, driver));
         // Configure the button bindings
         configureButtonBindings();
     }
