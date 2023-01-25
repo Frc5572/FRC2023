37e5c8b473ea2571582e7b0170e050c4233a7af1
Rango813 <omartinezhe6641@stu.neisd.net>
2023-01-25 09:00:09 -0600

    Javadoc

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -20,7 +20,7 @@ public class LEDs extends SubsystemBase {
      * constructs a LED Subsystem
      * 
      * @param length length of the addressable LEDS
-     * @param port port ID of PWM
+     * @param port port ID for PWM
      * 
      */
 
