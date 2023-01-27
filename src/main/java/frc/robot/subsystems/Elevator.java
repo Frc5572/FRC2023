efb5203dc63592ee76e02aa1cd8bf828a9979e9f
msmileyfort7307 <104444135+msmileyfort7307@users.noreply.github.com>
2023-01-26 19:03:14 -0600

    great framework will work

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import frc.robot.Constants;
 
@@ -18,7 +19,9 @@ public class Elevator extends ProfiledPIDSubsystem {
         new CANSparkMax(Constants.Elevator.leftElevatorMotorID, MotorType.kBrushless);
     private final CANSparkMax rightElevatorMotor =
         new CANSparkMax(Constants.Elevator.rightElevatorMotorID, MotorType.kBrushless);
+    private int elevatorEncoderTick;
     private final CANCoder elevatorCANCoder = new CANCoder(0);
+    private final Encoder test = new Encoder(null, null);
     private final ElevatorFeedforward elevatorFF =
         new ElevatorFeedforward(Constants.ElevatorPID.kSVolts, Constants.ElevatorPID.kGVolts,
             Constants.ElevatorPID.kVVoltsSecondsPerRotation);
@@ -33,10 +36,22 @@ public class Elevator extends ProfiledPIDSubsystem {
                 Constants.ElevatorPID.kMaxAccelerationRadPerSecSquared)));
     }
 
+    public void moveElevator(int currentEncoderTick, int stateEncoderTick) {
+        while (currentEncoderTick != stateEncoderTick) {
+            if (Math.abs(currentEncoderTick - stateEncoderTick) < 500) {
+                // Moves the motor mor in the positive direction
+            } else {
+                // Moves the motor mor in the negative direction
+            }
+        }
+    }
+
     @Override
     public double getMeasurement() {
-        // TODO Auto-generated method stub
-        return 0;
+        // Uses the .get() method to get the count of the encoder and stores it in
+        // elevatorEncoderTick which then is returned
+        elevatorEncoderTick = test.get();
+        return elevatorEncoderTick;
     }
 
     @Override
