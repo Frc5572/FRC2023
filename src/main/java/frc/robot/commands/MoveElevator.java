40abb156e4eda1e15a27d7b506b971e7b26698d4
msmileyfort7307 <104444135+msmileyfort7307@users.noreply.github.com>
2023-01-28 17:09:18 -0600

    Rough command outline made

---
+++ src/main/java/frc/robot/commands/MoveElevator.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Elevator;
+
+/**
+ * Creates a command for movine the elevator.
+ */
+public class MoveElevator extends CommandBase {
+
+    private CANSparkMax motor1 = Elevator.leftElevatorMotor;
+    private CANSparkMax motor2 = Elevator.rightElevatorMotor;
+    private DutyCycleEncoder dutyCycleEncoder = Elevator.elevatorEncoder;
+    private double encoderGoal;
+
+    /**
+     * Creates a command for movine the elevator.
+     *
+     * @param rotation Current rotaion value to determine where the motor is.
+     */
+    public MoveElevator(double rotation) {
+        this.encoderGoal = rotation;
+    }
+
+    @Override
+    public void execute() {
+        Elevator.runMotor(encVal(), encoderGoal);
+    }
+
+    @Override
+    public void end(boolean taskCompleted) {
+        if (taskCompleted == true) {
+            motor1.set(0);
+            motor2.set(0);
+        }
+    }
+
+    public double encVal() {
+        return (dutyCycleEncoder.getAbsolutePosition() - Constants.Elevator.encoderOffSet);
+    }
+}
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -14,23 +14,22 @@ import frc.robot.Constants;
  */
 public class Elevator extends SubsystemBase {
 
-    private final CANSparkMax leftElevatorMotor =
+    public static final CANSparkMax leftElevatorMotor =
         new CANSparkMax(Constants.Elevator.leftElevatorMotorID, MotorType.kBrushless);
-    private final CANSparkMax rightElevatorMotor =
+    public static final CANSparkMax rightElevatorMotor =
         new CANSparkMax(Constants.Elevator.rightElevatorMotorID, MotorType.kBrushless);
     private double elevatorElevatorRotation;
 
-    private final DutyCycleEncoder elevatorEncoder = new DutyCycleEncoder(99);
+    public static final DutyCycleEncoder elevatorEncoder = new DutyCycleEncoder(99);
 
     private final ElevatorFeedforward elevatorFF = new ElevatorFeedforward(
         Constants.Elevator.elevatorKSVolts, Constants.Elevator.elevatorKGVolts,
         Constants.Elevator.elevatorKVVoltsSecondsPerRotation);
 
-    private final ProfiledPIDController elevatorPID = new ProfiledPIDController(
+    private static final ProfiledPIDController elevatorPID = new ProfiledPIDController(
         Constants.Elevator.elevatorKP, Constants.Elevator.elevatorKI, Constants.Elevator.elevatorKD,
         new TrapezoidProfile.Constraints(Constants.Elevator.elevatorKMaxVelocityRadPerSecond,
             Constants.Elevator.elevatorKMaxAccelerationRadPerSecSquared));
-
 
 
     public Elevator() {
@@ -40,7 +39,7 @@ public class Elevator extends SubsystemBase {
     /**
      * Creates runMotor method to move the motors.
      */
-    public void runMotor(double currentVal, double goal) {
+    public static void runMotor(double currentVal, double goal) {
         leftElevatorMotor.set(elevatorPID.calculate(currentVal * 360, goal));
         rightElevatorMotor.set(elevatorPID.calculate(currentVal * 360, goal));
     }
