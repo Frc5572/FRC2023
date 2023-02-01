cce8ea9266d217c9db2d21c8486d25d78c469519
Alex Resnick <adr8292@gmail.com>
2023-02-01 14:01:18 -0600

    fix more linting

--- src/main/java/frc/robot/commands/DisabledInstantCommand.java
+++ src/main/java/frc/robot/commands/DisabledInstantCommand.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * A Command that runs instantly; it will initialize, execute once, and end on the same iteration of
  * the scheduler. Users can either pass in a Runnable and a set of requirements, or else subclass
  * this command if desired.
+ *
  * <p>
  * This command works while the robot is disabled.
  */
@@ -14,6 +15,7 @@ public class DisabledInstantCommand extends InstantCommand {
 
     /**
      * Creates a new InstantCommand that runs the given Runnable with the given requirements.
+     *
      * <p>
      * This command works while the robot is disabled.
      *
