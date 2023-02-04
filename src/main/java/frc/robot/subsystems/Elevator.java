abc56d6749f6ba515ae23010212c65c75cad8791
gvaldez7206 <90881143+gvaldez7206@users.noreply.github.com>
2023-02-04 12:28:14 -0600

    fixed linting errors

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -38,7 +38,7 @@ public class Elevator extends SubsystemBase {
     /**
      * Sets both of the elevator motors to a specific speed.
      * 
-     * @param speed the speed of the motors
+     * @param speed The desired speed of the motors.
      */
     public void set(double speed) {
         leftElevatorMotor.set(speed);
@@ -46,7 +46,7 @@ public class Elevator extends SubsystemBase {
     }
 
     /**
-     * @return return the current encoder rotation in constrast with the off set.
+     * @return return the current encoder rotation in contrast with the off set.
      */
     public double getElevatorRotation() {
 
