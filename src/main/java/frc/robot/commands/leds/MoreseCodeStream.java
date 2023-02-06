22ff71b7ba64849d6a0157cb4afe9052d30bf9ad
Alex Resnick <adr8292@gmail.com>
2023-02-05 19:55:18 -0600

    simplify codebase

--- src/main/java/frc/robot/commands/leds/MoreseCodeStream.java
+++ src/main/java/frc/robot/commands/leds/MoreseCodeStream.java
@@ -8,12 +8,15 @@ import frc.robot.subsystems.LEDs;
 
 /**
  * Command to flash the LED strip between 2 colors
+ *
+ * <p>
+ * DOES NOT WORK YET
  */
-public class MorseCodeFlash extends CommandBase {
+public class MoreseCodeStream extends CommandBase {
     private LEDs leds;
     private int index = 0;
     private ArrayList<Boolean> finalWord = new ArrayList<Boolean>();
-    private ArrayList<Color> finalStrip;
+    private ArrayList<Color> finalStrip = new ArrayList<Color>();
 
     /**
      * Command to flash the LED strip between 2 colors
@@ -22,11 +25,11 @@ public class MorseCodeFlash extends CommandBase {
      * @param word The word or sentance to flash in Morse Code
      * @param color The color to flash
      */
-    public MorseCodeFlash(LEDs leds, String word, Color color) {
+    public MoreseCodeStream(LEDs leds, String word, Color color) {
         this.leds = leds;
-        addRequirements(leds);
-        finalWord = MorseCode.generateDitArray(word, 10);
+        finalWord = MorseCode.generateDitArray(word, 1);
         finalStrip = MorseCode.generateDirColorArray(finalWord, color);
+        addRequirements(leds);
     }
 
     /**
@@ -35,7 +38,7 @@ public class MorseCodeFlash extends CommandBase {
      * @param leds LED Subsystem
      * @param word The word or sentance to flash in Morse Code
      */
-    public MorseCodeFlash(LEDs leds, String word) {
+    public MoreseCodeStream(LEDs leds, String word) {
         this(leds, word, Color.kRed);
     }
 
@@ -43,20 +46,33 @@ public class MorseCodeFlash extends CommandBase {
     public void initialize() {
         index = 0;
         leds.setColor(Color.kBlack);
+        System.out.println(finalStrip);
     }
 
 
     @Override
     public void execute() {
-        Color x = finalStrip.get(index);
-        leds.setColor(x);
+        leds.setColor(Color.kBlack);
+        for (int i = index; i >= 0; i--) {
+            if (index >= 0 && index < leds.getLength()) {
+                leds.setColor(i, finalStrip.get(index - i));
+            } else if (index >= leds.getLength()) {
+                leds.setColor(i - (leds.getLength() - index),
+                    finalStrip.get(i - (leds.getLength() - index)));
+            }
+        }
         index++;
+        // for (int i = index; i >= 0; i--) {
+        // int q = index - i;
+        // Color x = finalStrip.get(q);
+        // leds.setColor(q, x);
+        // }
     }
 
-    @Override
-    public boolean isFinished() {
-        return index >= finalStrip.size();
-    }
+    // @Override
+    // public boolean isFinished() {
+    // return index >= finalWord.size();
+    // }
 
     @Override
     public boolean runsWhenDisabled() {
