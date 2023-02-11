d598d6f8a411da72a551c554e3831fdb39ce0267
Alex Resnick <adr8292@gmail.com>
2023-02-11 16:41:47 -0600

    Fix conflicts

--- src/main/java/frc/robot/subsystems/DropIntake.java
+++ src/main/java/frc/robot/subsystems/DropIntake.java
@@ -14,24 +14,20 @@ import frc.robot.Constants;
  * Creates the subsystem for the dropdown intake.
  */
 public class DropIntake extends SubsystemBase {
-    private final CANSparkMax leftDropMotor = new CANSparkMax(
-        Constants.IntakeConstants.DropConstants.LEFT_DROP_MOTOR_ID, MotorType.kBrushless);
-    private final CANSparkMax rightDropMotor = new CANSparkMax(
-        Constants.IntakeConstants.DropConstants.RIGHT_DROP_MOTOR_ID, MotorType.kBrushless);
+    private final CANSparkMax leftDropMotor =
+        new CANSparkMax(Constants.Intake.DropDown.LEFT_DROP_MOTOR_ID, MotorType.kBrushless);
+    private final CANSparkMax rightDropMotor =
+        new CANSparkMax(Constants.Intake.DropDown.RIGHT_DROP_MOTOR_ID, MotorType.kBrushless);
     private final MotorControllerGroup dropdownMotors =
         new MotorControllerGroup(leftDropMotor, rightDropMotor);
-    private final CANSparkMax intakeMotor = new CANSparkMax(
-        Constants.IntakeConstants.DropConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+    private final CANSparkMax intakeMotor =
+        new CANSparkMax(Constants.Intake.DropDown.INTAKE_MOTOR_ID, MotorType.kBrushless);
     private final DutyCycleEncoder dropEncoder =
-        new DutyCycleEncoder(Constants.IntakeConstants.DropConstants.DROP_ENCODER_ID);
-    private final PIDController pid_controller =
-        new PIDController(Constants.IntakeConstants.DropConstants.PID.KP,
-            Constants.IntakeConstants.DropConstants.PID.KI,
-            Constants.IntakeConstants.DropConstants.PID.KD);
-    private final ArmFeedforward feedforward =
-        new ArmFeedforward(Constants.IntakeConstants.DropConstants.PID.KS,
-            Constants.IntakeConstants.DropConstants.PID.KG,
-            Constants.IntakeConstants.DropConstants.PID.KV);
+        new DutyCycleEncoder(Constants.Intake.DropDown.DROP_ENCODER_ID);
+    private final PIDController pid_controller = new PIDController(Constants.Intake.DropDown.PID.KP,
+        Constants.Intake.DropDown.PID.KI, Constants.Intake.DropDown.PID.KD);
+    private final ArmFeedforward feedforward = new ArmFeedforward(Constants.Intake.DropDown.PID.KS,
+        Constants.Intake.DropDown.PID.KG, Constants.Intake.DropDown.PID.KV);
 
     private final double dropEncoderOffset = 0.000;
     private final double defaultGoal = 0.0893 * 360;
@@ -47,39 +43,39 @@ public class DropIntake extends SubsystemBase {
      * Deploy the dropdown intake to the specified height for a cone. Stops when within threshold.
      */
     public void intakeConeDeploy() {
-        leftDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.DROP_VOLTS);
-        rightDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.DROP_VOLTS);
+        leftDropMotor.setVoltage(Constants.Intake.DropDown.DROP_VOLTS);
+        rightDropMotor.setVoltage(Constants.Intake.DropDown.DROP_VOLTS);
     }
 
     /**
      * Deploy the dropdown intake to the specified height for a cube. Stops when within threshold.
      */
     public void intakeCubeDeploy() {
-        leftDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.DROP_VOLTS);
-        rightDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.DROP_VOLTS);
+        leftDropMotor.setVoltage(Constants.Intake.DropDown.DROP_VOLTS);
+        rightDropMotor.setVoltage(Constants.Intake.DropDown.DROP_VOLTS);
     }
 
     /**
      * Retracts the dropdown intake to the default height. Stops when within threshold.
      */
     public void intakeRetract() {
-        leftDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.RETRACT_VOLTS);
-        rightDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.RETRACT_VOLTS);
+        leftDropMotor.setVoltage(Constants.Intake.DropDown.RETRACT_VOLTS);
+        rightDropMotor.setVoltage(Constants.Intake.DropDown.RETRACT_VOLTS);
     }
 
     public void stopDrop() {
-        leftDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.STOP_VOLTS);
-        rightDropMotor.setVoltage(Constants.IntakeConstants.DropConstants.STOP_VOLTS);
+        leftDropMotor.setVoltage(Constants.Intake.DropDown.STOP_VOLTS);
+        rightDropMotor.setVoltage(Constants.Intake.DropDown.STOP_VOLTS);
     }
 
     // Runs the intake at a specified speed.
     public void intake() {
-        intakeMotor.set(Constants.IntakeConstants.DropConstants.INTAKE_SPEED);
+        intakeMotor.set(Constants.Intake.DropDown.INTAKE_SPEED);
     }
 
     // Stops the intake from running.
     public void stop() {
-        intakeMotor.setVoltage(Constants.IntakeConstants.DropConstants.STOP_VOLTS);
+        intakeMotor.setVoltage(Constants.Intake.DropDown.STOP_VOLTS);
     }
 
     /**
