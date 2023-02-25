e2cce03c76d1be7707d6c86ff2fc91aead42ec12
Alex Resnick <adr8292@gmail.com>
2023-02-25 14:34:10 -0600

    updates

--- src/main/java/frc/robot/commands/MoveToScore.java
+++ src/main/java/frc/robot/commands/MoveToScore.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.lib.util.FieldConstants;
@@ -26,18 +27,29 @@ public class MoveToScore extends MoveToPos {
     @Override
     public void initialize() {
         int column = Robot.column;
+        int level = Robot.level;
         Pose2d basePos = new Pose2d();
-        int xOffset = -1;
+        Rotation2d rotation = Rotation2d.fromDegrees(180);
+        double xOffset = Units.inchesToMeters(50);
+        int redInvert = 1;
         if (DriverStation.getAlliance() == Alliance.Blue) {
             basePos = FieldConstants.aprilTags.get(8).toPose2d();
         } else if (DriverStation.getAlliance() == Alliance.Red) {
             basePos = FieldConstants.aprilTags.get(3).toPose2d();
+            rotation = Rotation2d.fromDegrees(0);
+            xOffset *= -1;
+            redInvert = -1;
         }
-        Map<Integer, Translation2d> columns = Map.of(0, new Translation2d(xOffset, -1), 1,
-            new Translation2d(xOffset, 0), 2, new Translation2d(xOffset, 1), 3,
-            new Translation2d(xOffset, 2), 4, new Translation2d(xOffset, 3), 5,
-            new Translation2d(xOffset, 4), 6, new Translation2d(xOffset, 5), 7,
-            new Translation2d(xOffset, 6), 8, new Translation2d(xOffset, 7));
-        pose2d = basePos.plus(new Transform2d(columns.get(column), Rotation2d.fromDegrees(180)));
+        Map<Integer, Translation2d> columns = Map.of(0,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * -1 * redInvert), 1,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 0 * redInvert), 2,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 1 * redInvert), 3,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 2 * redInvert), 4,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 3 * redInvert), 5,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 4 * redInvert), 6,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 5 * redInvert), 7,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 6 * redInvert), 8,
+            new Translation2d(xOffset, FieldConstants.Grids.nodeSeparationY * 7 * redInvert));
+        this.pose2d = basePos.plus(new Transform2d(columns.get(column), rotation));
     }
 }
