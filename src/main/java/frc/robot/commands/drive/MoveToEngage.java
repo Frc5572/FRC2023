7a328fef3adac41789ac42fa84730631217516ac
Owen <owenjflatman@gmail.com>
2023-03-01 16:27:58 -0600

    seems to be good

--- src/main/java/frc/robot/commands/drive/MoveToEngage.java
+++ src/main/java/frc/robot/commands/drive/MoveToEngage.java
@@ -1,9 +1,7 @@
 package frc.robot.commands.drive;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.lib.util.FieldConstants;
@@ -24,16 +22,16 @@ public class MoveToEngage extends SequentialCommandGroup {
     public MoveToEngage(Swerve swerve, Arm arm, WristIntake wristIntake) {
 
         ParallelRaceGroup dockArm = new DockArm(arm, wristIntake).withTimeout(2);
-        ConditionalCommand cond = new ConditionalCommand(
-            new MoveToPos(swerve,
-                () -> new Pose2d(swerve.getPose().getX(), swerve.getPose().getY(),
-                    new Rotation2d(0)),
-                true),
-            new MoveToPos(swerve, () -> new Pose2d(swerve.getPose().getX(), swerve.getPose().getY(),
-                new Rotation2d(Math.PI)), true),
-            () -> centerField(swerve));
+        // ConditionalCommand cond = new ConditionalCommand(
+        // new MoveToPos(swerve,
+        // () -> new Pose2d(swerve.getPose().getX(), swerve.getPose().getY(),
+        // new Rotation2d(0)),
+        // true),
+        // new MoveToPos(swerve, () -> new Pose2d(swerve.getPose().getX(), swerve.getPose().getY(),
+        // new Rotation2d(Math.PI)), true),
+        // () -> centerField(swerve));
         ClimbPlatform climbPlatform = new ClimbPlatform(swerve);
-        addCommands(dockArm, cond, climbPlatform);
+        addCommands(dockArm, climbPlatform);
     }
 
     private boolean centerField(Swerve swerve) {
