c98f680e4c95722e5ee033246342a65c1ac02c60
Alex Grinman <90881143+gvaldez7206@users.noreply.github.com>
2023-03-08 18:28:21 -0600

    starting moving position for second game piece

--- src/main/java/frc/robot/autos/SecondGamePiece.java
+++ src/main/java/frc/robot/autos/SecondGamePiece.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.lib.util.FieldConstants;
 import frc.robot.RobotContainer;
 import frc.robot.commands.drive.MoveToEngage;
+import frc.robot.commands.drive.MoveToPos;
 import frc.robot.commands.wrist.WristIntakeIn;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Swerve;
@@ -22,6 +24,8 @@ public class SecondGamePiece extends TrajectoryBase {
 
     private double maxVel = 0;
     private double maxAccel = 0;
+    Pose2d aprilTag8 = FieldConstants.aprilTags.get(8).toPose2d();
+    Pose2d aprilTag6 = FieldConstants.aprilTags.get(6).toPose2d();
 
     public SecondGamePiece(Swerve swerve, Arm arm, WristIntake intake) {
         super(swerve);
@@ -29,8 +33,13 @@ public class SecondGamePiece extends TrajectoryBase {
             PathPlanner.loadPath("Second Game Piece - 6", maxVel, maxAccel);
         PathPlannerTrajectory trajectory8 =
             PathPlanner.loadPath("Second Game Piece - 8", maxVel, maxAccel);
+
+        MoveToPos move6 = new MoveToPos(swerve, () -> get6position(), true);
+        MoveToPos move8 = new MoveToPos(swerve, () -> get8position(), true);
+
         PPSwerveControllerCommand secondGamePiece6 = baseSwerveCommand(trajectory6);
         PPSwerveControllerCommand secondGamePiece8 = baseSwerveCommand(trajectory8);
+
         HashMap<String, Command> eventMap = new HashMap<>();
         eventMap.put("Intake On", new WristIntakeIn(intake));
         FollowPathWithEvents secondGamePiece6Events =
@@ -38,12 +47,16 @@ public class SecondGamePiece extends TrajectoryBase {
         FollowPathWithEvents secondGamePiece8Events =
             new FollowPathWithEvents(secondGamePiece8, trajectory8.getMarkers(), eventMap);
         MoveToEngage engage = new MoveToEngage(swerve, arm, intake);
-        ConditionalCommand cond = new ConditionalCommand(secondGamePiece6Events,
+
+        ConditionalCommand cond = new ConditionalCommand(move6, move8, () -> chooseSide());
+
+        ConditionalCommand cond1 = new ConditionalCommand(secondGamePiece6Events,
             secondGamePiece8Events, () -> chooseSide());
-        ConditionalCommand cond1 = new ConditionalCommand(engage, new InstantCommand(),
+
+        ConditionalCommand cond2 = new ConditionalCommand(engage, new InstantCommand(),
             () -> RobotContainer.enableDockWidget.getBoolean(true));
 
-        addCommands(cond, cond1);
+        addCommands(cond, cond1, cond2);
 
     }
 
@@ -57,5 +70,28 @@ public class SecondGamePiece extends TrajectoryBase {
         double midPlatform =
             Units.inchesToMeters(59.39) + FieldConstants.Community.chargingStationWidth / 2;
         return pose2d.getY() > midPlatform;
+    }
+
+    /**
+     * Create the Pose2d closer to scoring table
+     *
+     * @return Pose2d on scoring table side
+     */
+    private Pose2d get8position() {
+        double x = aprilTag8.getX() + Units.inchesToMeters(50);
+        double y = Units.inchesToMeters(59.39 / 2);
+        return new Pose2d(x, y, Rotation2d.fromDegrees(180));
+    }
+
+
+    /**
+     * Create the Pose2d closer to feeder station
+     *
+     * @return Pose2d on feeder station side
+     */
+    private Pose2d get6position() {
+        double x = aprilTag6.getX() + Units.inchesToMeters(50);
+        double y = aprilTag6.getY() + Units.inchesToMeters(26);
+        return new Pose2d(x, y, Rotation2d.fromDegrees(180));
     }
 }
