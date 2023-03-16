31c2006840ea02129f7347600728600b36d4cc10
Owen <owenjflatman@gmail.com>
2023-03-16 14:15:47 -0500

    Widget changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,10 +56,10 @@ public class RobotContainer {
             .withPosition(6, 0).withSize(2, 2).withProperties(
                 Map.of("Number of columns", 2, "Number of rows", 1, "Label position", "TOP"));
     public GenericEntry levelWidget = targetGrid.add("Level", Robot.level)
-        .withWidget(BuiltInWidgets.kNumberBar).withProperties(Map.of("Min", 0, "Max", 2, "Center",
-            0, "Num tick marks", 3, "Show Text", false, "Orientation", "VERTICAL"))
+        .withWidget(BuiltInWidgets.kNumberBar).withProperties(Map.of("Min", -1, "Max", 2, "Center",
+            -1, "Num tick marks", 4, "Show Text", false, "Orientation", "VERTICAL"))
         .getEntry();
-    public GenericEntry columnWidet = targetGrid.add("Column", Robot.column)
+    public GenericEntry columnWidget = targetGrid.add("Column", Robot.column)
         .withWidget(BuiltInWidgets.kNumberBar).withProperties(Map.of("Min", 0, "Max", 8, "Center",
             0, "Num tick marks", 5, "Show Text", false, "Orientation", "VERTICAL"))
         .getEntry();
@@ -126,6 +126,7 @@ public class RobotContainer {
         autoChooser.addOption("Move To Score", new MoveToScore(s_Swerve, s_Arm, s_wristIntake));
 
         levelsChooser.setDefaultOption("0", 0);
+        levelsChooser.addOption("-1", -1);
         levelsChooser.addOption("0", 0);
         levelsChooser.addOption("1", 1);
         levelsChooser.addOption("2", 2);
