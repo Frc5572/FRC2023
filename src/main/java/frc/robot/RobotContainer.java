d3ea6b2e8b4c3b5efb95cd0f2143905ec6d070d3
Alex Grinman <90881143+gvaldez7206@users.noreply.github.com>
2023-03-16 21:50:44 -0500

    changes from day 1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,8 +32,8 @@ import frc.robot.commands.drive.MoveToEngage;
 import frc.robot.commands.drive.MoveToScore;
 import frc.robot.commands.drive.TeleopSwerve;
 import frc.robot.commands.leds.FlashingLEDColor;
+import frc.robot.commands.leds.MovingColorLEDs;
 import frc.robot.commands.leds.PoliceLEDs;
-import frc.robot.commands.leds.RainbowLEDs;
 import frc.robot.commands.wrist.WristIntakeIn;
 import frc.robot.commands.wrist.WristIntakeRelease;
 import frc.robot.subsystems.Arm;
@@ -146,7 +146,8 @@ public class RobotContainer {
         autoChooser.addOption("Score 1 Dock", new Score1Dock(s_Swerve, s_Arm, s_wristIntake));
         autoChooser.addOption("Score 1", new Score1(s_Swerve, s_Arm, s_wristIntake));
         // Configure the button bindings
-        leds.setDefaultCommand(new RainbowLEDs(leds));
+        leds.setDefaultCommand(new MovingColorLEDs(leds, Color.kRed, 8, false));
+        // leds.setDefaultCommand(new Twinkle(leds, 60, new Color[] {Color.kRed}));
         configureButtonBindings();
     }
 
