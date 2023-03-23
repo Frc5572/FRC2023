399ea4eba833e9f1f0108d2e03e3f8dfd76e98d8
Owen <owenjflatman@gmail.com>
2023-03-23 16:44:09 -0500

    Progress

--- src/main/java/frc/robot/commands/arm/ArmIntake.java
+++ src/main/java/frc/robot/commands/arm/ArmIntake.java
@@ -8,8 +8,8 @@ import frc.robot.subsystems.Arm;
  * This command will move the arm to a requested angle.
  */
 public class ArmIntake extends SequentialCommandGroup {
-    private double armAngle = -55.0;
-    private double wristAngle = -17;
+    private double armAngle = -40.0;
+    private double wristAngle = -30;
 
     /**
      * Requirements for the command.
