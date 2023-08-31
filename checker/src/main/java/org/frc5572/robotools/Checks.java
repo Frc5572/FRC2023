27d38ce769b2326e48900cb029c66c7aa4133073
Wilson Watson <wwatso1tech@gmail.com>
2023-08-31 16:14:51 -0500

    fix testtool

--- checker/src/main/java/org/frc5572/robotools/Checks.java
+++ checker/src/main/java/org/frc5572/robotools/Checks.java
@@ -3,9 +3,10 @@ package org.frc5572.robotools;
 import org.frc5572.robotools.checks.Check;
 import org.frc5572.robotools.checks.IOCheck;
 
+/** All checks to run. */
 public class Checks {
 
-    private static final Check[] CHECKS = new Check[] {new IOCheck(),};
+    private static final Check[] CHECKS = new Check[] {new IOCheck()};
 
     /** Run through all checks */
     public static void process(CompilationData data) {
