a3d06259801699e32581c04a925d0bd1022ce145
gvaldez7206 <90881143+gvaldez7206@users.noreply.github.com>
2023-09-06 18:13:37 -0500

    Starting IO class

---
+++ src/main/java/frc/robot/subsystems/Arm/ArmIO.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.Arm;
+
+public interface ArmIO {
+
+    public static class ArmInputs {
+        double encoder;
+        double voltage;
+        boolean isExtended;
+    }
+
+    public static void setExtension() {
+
+    }
+
+    public static void setVoltage() {
+
+    }
+
+    public static double getAngle(){
+        return 
+    }
+}
